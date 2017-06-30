dc9330690215b300afda3bc2050cefbca9c1e768
Marcin Wegrzyn <wireamg@gmail.com>
2017-07-01 00:50:54 +0200

    add image intent

--- app/src/main/java/com/example/marcin/wegrzyn/inventoryapp/AddProductActivity.java
+++ app/src/main/java/com/example/marcin/wegrzyn/inventoryapp/AddProductActivity.java
@@ -1,10 +1,40 @@
 package com.example.marcin.wegrzyn.inventoryapp;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+
+import static com.example.marcin.wegrzyn.inventoryapp.Data.ProductConrtact.ProductEntry;
 
 public class AddProductActivity extends AppCompatActivity {
+
+
+    public static final String TAG = AddProductActivity.class.getName();
+
+    static final int IMAGE_REQUEST_CODE = 1;
+    public static final String SAVE_URI = "SaveUri";
+    private ImageView imageView;
+    private TextView emptyView;
+    private Uri uri;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if(uri!= null) outState.putString(SAVE_URI,uri.toString());
+        super.onSaveInstanceState(outState);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +43,77 @@ public class AddProductActivity extends AppCompatActivity {
 
         setTitle("add Product");
 
+        imageView = (ImageView) findViewById(R.id.image);
+        emptyView = (TextView) findViewById(R.id.textEmpty);
+
+        if(savedInstanceState!=null){
+            uri = Uri.parse(savedInstanceState.getString(SAVE_URI,null));
+            showImage();
+        }
         EditText editText = (EditText) findViewById(R.id.editName);
         editText.getText();
+
+        if(uri!=null)showImage();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.add){
+            Toast.makeText(this,"add",Toast.LENGTH_SHORT).show();
+            return true;
+
+        }else if(id == R.id.addImage){
+            Intent getImageIntent = new Intent(Intent.ACTION_PICK);
+            getImageIntent.setType("image/*");
+            startActivityForResult(getImageIntent, IMAGE_REQUEST_CODE);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==IMAGE_REQUEST_CODE&&resultCode==RESULT_OK&&data!=null){
+            uri = data.getData();
+            showImage();
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    private void showImage(){
+        Bitmap bitmap = null;
+        try {
+            bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        imageView.setImageBitmap(bitmap);
+        emptyView.setVisibility(View.INVISIBLE);
+    }
+
+    private void testInsert(String name, int quantity, int price, String desc, String img, String supplier) {
+
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(ProductEntry.COLUMN_NAME, name);
+        contentValues.put(ProductEntry.COLUMN_QUANTITY, quantity);
+        contentValues.put(ProductEntry.COLUMN_PRICE, price);
+        contentValues.put(ProductEntry.COLUMN_DESC, desc);
+        contentValues.put(ProductEntry.COLUMN_IMAGE, img);
+        contentValues.put(ProductEntry.COLUMN_SUPPLIER, supplier);
+
+
+        Uri uri = getContentResolver().insert(ProductEntry.CONTENT_URI, contentValues);
+        Toast.makeText(getBaseContext(), "Test Insert: " + uri, Toast.LENGTH_SHORT).show();
+
+    }
+
 }
--- app/src/main/java/com/example/marcin/wegrzyn/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/marcin/wegrzyn/inventoryapp/MainActivity.java
@@ -12,6 +12,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -21,6 +23,7 @@ import android.widget.Toast;
 import com.example.marcin.wegrzyn.inventoryapp.Data.ProductConrtact.ProductEntry;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
 
 
     public static final String TAG = MainActivity.class.getSimpleName();
@@ -39,8 +42,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         actionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-//                testInsert("Precel", 50, 5, "bialy", "obraz", "Biedronka");
 
                 Intent intent = new Intent(MainActivity.this, AddProductActivity.class);
                 startActivity(intent);
@@ -96,6 +97,22 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public void onLoaderReset(Loader<Cursor> loader) {
         Log.d(TAG, "onLoaderReset");
         productCursorAdapter.swapCursor(null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.dummy_data){
+            testInsert("bread",10, 5, "white", "Image", "Lidl");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void testInsert(String name, int quantity, int price, String desc, String img, String supplier) {
